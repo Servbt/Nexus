f67e7681386195f961b6d7b53e0c642aa4d8e8b5
alxsaunders <alexsaunders242@gmail.com>
2022-08-07 20:40:18 -0400

    fix single game

---
+++ public/javascript/single-game.js
@@ -0,0 +1,38 @@
+var coversingle = document.querySelector("#gamename")
+var coverspot = document.querySelector('#gamecover')
+
+
+var apiUrlsingle = `https://api.twitch.tv/helix/games?name=${coversingle.innerText}`
+
+function getGameImage() {
+    fetch(apiUrlsingle, {  
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer xgzr2w68p7c7ivfgcuewooskfmqpyt",
+        "Client-ID": "fj1zbvow6f5o4tbej4txgkv0qbk0ww",
+      }
+    })
+    .then(function (response) {
+        if (response.ok) {
+            console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                        displayGame(data.data[0].id)
+                
+                    
+                })
+            }
+        })
+    }
+
+    function displayGame(coverArt) {
+
+        let gameImage = `https://static-cdn.jtvnw.net/ttv-boxart/${coverArt}_IGDB-720x980.jpg`;
+      
+        
+            coverspot.innerHTML = `<img src="${gameImage} alt="game Image goes here">`
+         
+        
+      }
+
+      getGameImage(coverspot)
