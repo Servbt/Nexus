6d8fbd27ce512082b9a9fbdb27fd9883fe3ca818
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-09-19 14:20:25 -0400

    trying to redeploy nexus on my laptop

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -6,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
