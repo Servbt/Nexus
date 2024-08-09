cdf71d93a3c5ad1bce786ba30be0e5ed3792331d
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-08-08 22:09:49 -0400

    add game seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,15 +3,17 @@ const { User, Game, Tag, Review } = require('../models');
 
 const userData = require('./userData.json');
 const reviewData = require('./reviewData.json');
+const gameData = require('./gameData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: false });
-  const users = await User.bulkCreate(userData);
-  const reviews = await Review.bulkCreate(reviewData);
+  // const users = await User.bulkCreate(userData);
+  const games = await Game.bulkCreate(gameData);
+  // const reviews = await Review.bulkCreate(reviewData);
   // await User.bulkCreate(userData, {
   //   individualHooks: true,
   //   returning: true,
-
+  console.log('Database seeded successfully');
   process.exit(0);
 };
 
