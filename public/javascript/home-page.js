71a39eec820d7cbcca86b1c19c567c6772a81fac
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-08-14 23:59:33 -0400

    New Twitch Auth Key

--- public/javascript/home-page.js
+++ public/javascript/home-page.js
@@ -1,6 +1,6 @@
 const input = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
-let myKey = "Bearer f2ti3uhxxe45ac84587ydfsuy50y33";
+let myKey = "Bearer NONONONONO";
 let myClient = "fj1zbvow6f5o4tbej4txgkv0qbk0ww"
 
 const expand = (e) => {
